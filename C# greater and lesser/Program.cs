b30c9cc8c0b6709a548653199f74ce887d6c1ed1
Abdul asik raja <asikraja2001@gmail.com>
2024-12-18 22:28:14 +0530

    Add project files.

---
+++ C# greater and lesser/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__greater_and_lesser
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number > 0)
+            {
+                Console.WriteLine("Positive");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("Negative");
+            }
+            else
+            {
+                Console.WriteLine("Zero");
+            }
+        }
+    }
+}
